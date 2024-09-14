2788fddd23dad868ee55c732c4a27f3169a4e49b
BumMuncher85 <noah.freedley@mymail.champlain.edu>
2024-09-14 18:05:39 -0400

    finished hello triangle namespace alteration

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -1,161 +1,85 @@
-#include <stdio.h>
-#include <math.h>
+#include "../core/Shader/shader.hpp"
 
 #include <ew/external/glad.h>
 #include <ew/ewMath/ewMath.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <iostream>
 
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
-float verticies[] = {
-	//   X      Y     Z     R     G     B     A
-	   -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
-		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f
+float vertices[] = {
+    //   X      Y     Z     R     G     B     A
+    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+     0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f
 };
 
-const char* vertexShaderSource = R"(
-#version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec4 aColor;
-out vec4 Color;
-uniform float time;
-void main()
-{
-   Color = aColor;
-   gl_Position = vec4(aPos.x, aPos.y + sin((time+aPos.x)*2.5)/5, aPos.z, 1.0);
-};
-)";
-//ABOVE CAN USE R"()"; to avoid using \n and format as a multiline string by default
-
-const char* fragmentShaderSource = R"(
-#version 330 core
-out vec4 FragColor;
-in vec4 Color;
-uniform float updateColor;
-void main()
-{
-    FragColor = Color * updateColor;
-} 
-)";
+const char* vertexShaderPath = "assets/vertex.vert";
+const char* fragmentShaderPath = "assets/fragment.frag";
 
 int main() {
-	
-	printf("Initializing...");
-	if (!glfwInit()) {
-		printf("GLFW failed to init!");
-		return 1;
-	}
+    std::cout << "Initializing..." << std::endl;
+    if (!glfwInit()) {
+        std::cerr << "GLFW failed to initialize!" << std::endl;
+        return -1;
+    }
 
-	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
-	if (window == NULL) {
-		printf("GLFW failed to create window");
-		return 1;
-	}
-	glfwMakeContextCurrent(window);
-	if (!gladLoadGL(glfwGetProcAddress)) {
-		printf("GLAD Failed to load GL headers");
-		return 1;
-	}
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "GLFW failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        std::cerr << "GLAD failed to load GL headers" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
-	//Initialization goes here!
-	//VERTEX DATA!!!
-	//vertex array object
-	
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
+    // Initialization
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
 
-	//Create Vertex Buffer Object
-	unsigned int VBO;  	
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STREAM_DRAW); 	//Put Data Into Curretly Bound VBO
-	//newer approach ^^^
-	//glNamedBufferData(VBO, sizeof(verticies), verticies, GL_STATIC_DRAW);
-	
-	//position (XYZ)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	//color (RGBA)
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3*sizeof(float)));
-	glEnableVertexAttribArray(1);
+    // Vertex attribute pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
-	int  success;
-	char infoLog[512]; //enough space to put output (to dump info log)
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s", infoLog);
-		//std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	} else {
-		printf("\nSHADER::VERTEX::COMPILATION_SUCCESS");
-	}
-	//Create and compile fragment shader
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s", infoLog);
-	} else {
-		printf("\nSHADER::FRAGMENT::COMPILATION_SUCCESS\n");
-	}
+    // Shader setup
+    SHADER_NS::Shader shader(vertexShaderPath, fragmentShaderPath);
 
-	//create and link program
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		printf("ERROR:SHADER::PROGRAM::LINKING_FAILED\n%s", infoLog);
-	}
-	else {
-		printf("SHADER::PROGRAM::LINKING_SUCCESS\n");
-	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+    // Main render loop
+    while (!glfwWindowShouldClose(window)) {
+        // Clear framebuffer
+        glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Activate shader
+        shader.use();
+
+        // Update uniforms
+        float time = glfwGetTime();
+        shader.setFloat("updateColor", abs(sin(time)));
+        shader.setFloat("time", time);
 
 
-	//Render loop
-	while (!glfwWindowShouldClose(window)) {
-		//Clear framebuffer
-		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+        // Render
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		//activating shader
-		glUseProgram(shaderProgram);
-
-		//update things
-		float time = glfwGetTime();
-
-		//color
-		unsigned int vertexColorLocation = glGetUniformLocation(shaderProgram, "updateColor");
-		glUniform1f(vertexColorLocation, abs(sin(time)));
-
-		//position
-		int vertexTimeLocation = glGetUniformLocation(shaderProgram, "time");
-		glUniform1f(vertexTimeLocation, time);
-
-		//render things
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
-		//swap buffers and poll IO events
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-	printf("Shutting down...");
+        // Swap buffers and poll IO events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    std::cout << "Shutting down..." << std::endl;
 }
